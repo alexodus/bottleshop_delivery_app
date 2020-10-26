2d13362372da22f41f7f339c2b6da0391c97e617
Johny <johny@cloudis.dev>
2020-10-26 12:40:09 +0100

    fix(auth): fix autoredirect after sign up
    Signed-off-by: Johny <johny@cloudis.dev>

--- lib/src/features/auth/presentation/pages/sign_in_page.dart
+++ lib/src/features/auth/presentation/pages/sign_in_page.dart
@@ -4,7 +4,6 @@ import 'package:bottleshopdeliveryapp/src/core/presentation/res/validator.dart';
 import 'package:bottleshopdeliveryapp/src/core/presentation/widgets/form_input_field_with_icon.dart';
 import 'package:bottleshopdeliveryapp/src/core/presentation/widgets/loader_widget.dart';
 import 'package:bottleshopdeliveryapp/src/features/auth/presentation/pages/reset_password_page.dart';
-import 'package:bottleshopdeliveryapp/src/features/auth/presentation/pages/sign_up_page.dart';
 import 'package:bottleshopdeliveryapp/src/features/auth/presentation/providers/auth_providers.dart';
 import 'package:bottleshopdeliveryapp/src/features/auth/presentation/widgets/social_media.dart';
 import 'package:flutter/material.dart';
@@ -24,15 +23,16 @@ class SignInPage extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
+    final signIn = useState<bool>(true);
     final showPassword = useState<bool>(false);
     final email = useTextEditingController();
     final password = useTextEditingController();
+    final passwordRepeat = useTextEditingController();
     final loading =
         useProvider(userRepositoryProvider.select((value) => value.isLoading));
     final error =
         useProvider(userRepositoryProvider.select((value) => value.error));
     final isAppleAvailable = useProvider(appleSignInAvailableProvider);
-    logger.v('userRepoErrors: $error');
     return Scaffold(
       key: _scaffoldKey,
       backgroundColor: Theme.of(context).accentColor,
@@ -45,43 +45,284 @@ class SignInPage extends HookWidget {
             child: Column(
               mainAxisSize: MainAxisSize.max,
               children: <Widget>[
-                Stack(
-                  children: <Widget>[
-                    buildFormContainerTop(context: context),
-                    Container(
-                      width: double.infinity,
-                      padding:
-                          EdgeInsets.symmetric(vertical: 30, horizontal: 30),
-                      margin:
-                          EdgeInsets.symmetric(vertical: 85, horizontal: 20),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: Theme.of(context).primaryColor,
-                        boxShadow: [
-                          BoxShadow(
-                              color:
-                                  Theme.of(context).hintColor.withOpacity(0.2),
-                              offset: Offset(0, 10),
-                              blurRadius: 20)
-                        ],
-                      ),
-                      child: SignInWidget(
-                        email: email,
-                        showPassword: showPassword,
-                        password: password,
-                        formKey: _formKey,
-                        isAppleAvailable: isAppleAvailable,
-                        authCallback: authCallback,
-                      ),
-                    ),
-                  ],
+                AnimatedSwitcher(
+                  duration: Duration(milliseconds: 500),
+                  child: signIn.value
+                      ? Stack(
+                          key: UniqueKey(),
+                          children: <Widget>[
+                            buildFormContainerTop(context: context),
+                            Container(
+                              width: double.infinity,
+                              padding: EdgeInsets.symmetric(
+                                  vertical: 30, horizontal: 30),
+                              margin: EdgeInsets.symmetric(
+                                  vertical: 85, horizontal: 20),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                color: Theme.of(context).primaryColor,
+                                boxShadow: [
+                                  BoxShadow(
+                                      color: Theme.of(context)
+                                          .hintColor
+                                          .withOpacity(0.2),
+                                      offset: Offset(0, 10),
+                                      blurRadius: 20)
+                                ],
+                              ),
+                              child: SignInWidget(
+                                email: email,
+                                showPassword: showPassword,
+                                password: password,
+                                formKey: _formKey,
+                                isAppleAvailable: isAppleAvailable,
+                                authCallback: authCallback,
+                              ),
+                            ),
+                          ],
+                        )
+                      : Stack(
+                          key: UniqueKey(),
+                          children: <Widget>[
+                            Container(
+                              width: double.infinity,
+                              padding: EdgeInsets.symmetric(
+                                  vertical: 30, horizontal: 20),
+                              margin: EdgeInsets.symmetric(
+                                  vertical: 65, horizontal: 50),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                color: Theme.of(context)
+                                    .primaryColor
+                                    .withOpacity(0.6),
+                              ),
+                            ),
+                            Container(
+                              width: double.infinity,
+                              padding: EdgeInsets.symmetric(
+                                  vertical: 30, horizontal: 30),
+                              margin: EdgeInsets.symmetric(
+                                  vertical: 85, horizontal: 20),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                color: Theme.of(context).primaryColor,
+                                boxShadow: [
+                                  BoxShadow(
+                                      color: Theme.of(context)
+                                          .hintColor
+                                          .withOpacity(0.2),
+                                      offset: Offset(0, 10),
+                                      blurRadius: 20)
+                                ],
+                              ),
+                              child: Column(
+                                children: <Widget>[
+                                  SizedBox(height: 25),
+                                  Text(S.of(context).sign_up,
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .headline2),
+                                  SizedBox(height: 20),
+                                  FormInputFieldWithIcon(
+                                    style: TextStyle(
+                                        color: Theme.of(context).accentColor),
+                                    keyboardType: TextInputType.emailAddress,
+                                    decoration: InputDecoration(
+                                        hintText: 'Email Address',
+                                        hintStyle: Theme.of(context)
+                                            .textTheme
+                                            .bodyText2
+                                            .merge(
+                                              TextStyle(
+                                                  color: Theme.of(context)
+                                                      .accentColor),
+                                            ),
+                                        enabledBorder: UnderlineInputBorder(
+                                            borderSide: BorderSide(
+                                                color: Theme.of(context)
+                                                    .accentColor
+                                                    .withOpacity(0.2))),
+                                        focusedBorder: UnderlineInputBorder(
+                                          borderSide: BorderSide(
+                                              color: Theme.of(context)
+                                                  .accentColor),
+                                        ),
+                                        prefixIcon: Icon(
+                                          Icons.mail_outline,
+                                          color: Theme.of(context).accentColor,
+                                        )),
+                                    controller: email,
+                                    validator: Validator().email,
+                                    onChanged: (value) => null,
+                                    onSaved: (value) => email.text = value,
+                                    maxLines: 1,
+                                  ),
+                                  SizedBox(height: 20),
+                                  FormInputFieldWithIcon(
+                                    style: TextStyle(
+                                        color: Theme.of(context).accentColor),
+                                    decoration: InputDecoration(
+                                      hintText: 'Password',
+                                      hintStyle: Theme.of(context)
+                                          .textTheme
+                                          .bodyText2
+                                          .merge(
+                                            TextStyle(
+                                                color: Theme.of(context)
+                                                    .accentColor),
+                                          ),
+                                      enabledBorder: UnderlineInputBorder(
+                                          borderSide: BorderSide(
+                                              color: Theme.of(context)
+                                                  .accentColor
+                                                  .withOpacity(0.2))),
+                                      focusedBorder: UnderlineInputBorder(
+                                        borderSide: BorderSide(
+                                            color:
+                                                Theme.of(context).accentColor),
+                                      ),
+                                      prefixIcon: Icon(
+                                        Icons.lock,
+                                        color: Theme.of(context).accentColor,
+                                      ),
+                                      suffixIcon: IconButton(
+                                        onPressed: () => showPassword.value =
+                                            !showPassword.value,
+                                        color: Theme.of(context)
+                                            .accentColor
+                                            .withOpacity(0.4),
+                                        icon: Icon(showPassword.value
+                                            ? Icons.visibility
+                                            : Icons.visibility_off),
+                                      ),
+                                    ),
+                                    controller: password,
+                                    validator: Validator().password,
+                                    obscureText: !showPassword.value,
+                                    maxLines: 1,
+                                    onChanged: (value) => null,
+                                    onSaved: (value) => password.text = value,
+                                  ),
+                                  SizedBox(height: 20),
+                                  FormInputFieldWithIcon(
+                                    style: TextStyle(
+                                        color: Theme.of(context).accentColor),
+                                    decoration: InputDecoration(
+                                      hintText: 'Password',
+                                      hintStyle: Theme.of(context)
+                                          .textTheme
+                                          .bodyText2
+                                          .merge(
+                                            TextStyle(
+                                                color: Theme.of(context)
+                                                    .accentColor),
+                                          ),
+                                      enabledBorder: UnderlineInputBorder(
+                                          borderSide: BorderSide(
+                                              color: Theme.of(context)
+                                                  .accentColor
+                                                  .withOpacity(0.2))),
+                                      focusedBorder: UnderlineInputBorder(
+                                        borderSide: BorderSide(
+                                            color:
+                                                Theme.of(context).accentColor),
+                                      ),
+                                      prefixIcon: Icon(
+                                        Icons.lock,
+                                        color: Theme.of(context).accentColor,
+                                      ),
+                                      suffixIcon: IconButton(
+                                        onPressed: () => showPassword.value =
+                                            !showPassword.value,
+                                        color: Theme.of(context)
+                                            .accentColor
+                                            .withOpacity(0.4),
+                                        icon: Icon(showPassword.value
+                                            ? Icons.visibility
+                                            : Icons.visibility_off),
+                                      ),
+                                    ),
+                                    controller: passwordRepeat,
+                                    validator: (val) {
+                                      var pwdVal = Validator().password(val);
+                                      if (pwdVal == null) {
+                                        if (val == password.text) {
+                                          return null;
+                                        }
+                                        return "Passwords don't match";
+                                      }
+                                      return pwdVal;
+                                    },
+                                    obscureText: !showPassword.value,
+                                    maxLines: 1,
+                                    onChanged: (value) => null,
+                                    onSaved: (value) => password.text = value,
+                                  ),
+                                  SizedBox(height: 40),
+                                  FlatButton(
+                                    padding: EdgeInsets.symmetric(
+                                        vertical: 12, horizontal: 70),
+                                    onPressed: () async {
+                                      if (_formKey.currentState.validate()) {
+                                        final authResult = await context
+                                            .read(userRepositoryProvider)
+                                            .signUpWithEmailAndPassword(
+                                                email.text, password.text);
+                                        logger.v('signUp result: $authResult');
+                                      }
+                                    },
+                                    child: Text(
+                                      S.of(context).sign_up,
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .headline6
+                                          .merge(
+                                            TextStyle(
+                                                color: Theme.of(context)
+                                                    .primaryColor),
+                                          ),
+                                    ),
+                                    color: Theme.of(context).accentColor,
+                                    shape: StadiumBorder(),
+                                  ),
+                                  SizedBox(height: 50),
+                                  Text(
+                                    S.of(context).or_social_media,
+                                    style:
+                                        Theme.of(context).textTheme.bodyText2,
+                                  ),
+                                  SizedBox(height: 20),
+                                  isAppleAvailable.when(
+                                    data: (isAppleSupported) =>
+                                        SocialMediaWidget(
+                                            isAppleSupported: isAppleSupported,
+                                            authResultCallback: (res) =>
+                                                logger.v('result: $res')),
+                                    loading: () => Container(
+                                      height: 45,
+                                      width: double.infinity,
+                                      child: CircularProgressIndicator(),
+                                    ),
+                                    error: (_, __) => SocialMediaWidget(
+                                        authResultCallback: (res) =>
+                                            logger.v('result: $res')),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                  transitionBuilder: (widget, animation) => ScaleTransition(
+                    scale: animation,
+                    child: widget,
+                  ),
                 ),
                 buildPrimaryButton(
-                    context,
-                    S.of(context).dontHaveAnAccount,
-                    S.of(context).sign_up,
-                    () => Navigator.pushReplacementNamed(
-                        context, SignUpPage.routeName)),
+                  context,
+                  S.of(context).dontHaveAnAccount,
+                  S.of(context).sign_up,
+                  () => signIn.value = !signIn.value,
+                ),
               ],
             ),
           ),
